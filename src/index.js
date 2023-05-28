2721aa7a276ee0014cc1d57c31c28e6b36c7728e
Alberuni Azad <azad@hastechit.com>
2023-05-28 15:34:17 +0600

    Complete and practiced react components

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
+import React from 'react';
+
 
 const index = 0;
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -13,17 +14,30 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 // 	);
 // }
 
-// Class component
-class Clock{
-	print(){
+// Class component : Way 1
+// class Clock{
+// print(){
+// 	return (
+// 		<h1 className='heading' tabIndex={index}>
+// 		<span className='text'>hello {new Date().toLocaleTimeString()}</span>
+// 		</h1>
+// 	);
+// }
+// }
+
+// const ClockComponent = new Clock();
+
+// root.render(ClockComponent.print());
+
+// Class component : Way 2
+class Clock extends React.Component{
+	render(){
 		return (
 			<h1 className='heading' tabIndex={index}>
-				<span className='text'>hello {new Date().toLocaleTimeString()}</span>
+			<span className='text'>hello {new Date().toLocaleTimeString(this.props.locale)}</span>
 			</h1>
 		);
 	}
 }
 
-const ClockComponent = new Clock();
-
-root.render(ClockComponent.print());
+root.render(<Clock locale="bn-BD" />);
